7d3ba29bcb6ef2b6f55658e65af86fa68ff2e441
Arto Bendiken <arto@bendiken.net>
2010-09-19 23:41:49 +0200

    Ensured that the QtRuby bindings are loaded up for the GUI app.

--- lib/opencoinage/wallet/gui.rb
+++ lib/opencoinage/wallet/gui.rb
@@ -1,3 +1,9 @@
+begin
+  require 'Qt4' # @see http://rubygems.org/gems/qtbindings
+rescue LoadError
+  abort "OpenCoinage requires the QtRuby bindings (hint: `gem install qtbindings')."
+end
+
 module OpenCoinage::Wallet
   ##
   # The graphical user interface (GUI) wallet application.
