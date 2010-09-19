fbad61f7b26daa20597b78eb9570a396b7682bae
Arto Bendiken <arto@bendiken.net>
2010-09-19 22:52:05 +0200

    Ensured that the SQLite3/Ruby gem is always loaded up.

--- lib/opencoinage/wallet.rb
+++ lib/opencoinage/wallet.rb
@@ -1,4 +1,10 @@
-require 'opencoinage'
+require 'opencoinage' # @see http://rubygems.org/gems/opencoinage
+
+begin
+  require 'sqlite3'   # @see http://rubygems.org/gems/sqlite3-ruby
+rescue LoadError
+  abort "OpenCoinage requires the SQLite3/Ruby gem (hint: `gem install sqlite3-ruby')."
+end
 
 module OpenCoinage
   module Wallet
